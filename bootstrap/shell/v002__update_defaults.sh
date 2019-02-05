ee659eefd6be744604526ebc9eed3ae795572638
Karthik.K <karthik.k1@olacabs.com>
2019-02-05 14:47:08 +0530

    added tooltip for flink master

--- bootstrap/shell/v002__update_defaults.sh
+++ bootstrap/shell/v002__update_defaults.sh
@@ -169,13 +169,12 @@ function update_bundles {
     #put_topology_component_bundle /streams/componentbundles/SINK ${component_dir}/sinks/druid-sink-topology-component.json DRUID STORM
     # === Topology ===
     #put_topology_component_bundle /streams/componentbundles/TOPOLOGY ${component_dir}/topology/storm-topology-component.json TOPOLOGY STORM
-    put_topology_component_bundle /streams/componentbundles/TOPOLOGY ${component_dir}/topology/beam-topology-component.json TOPOLOGY
     # === Service Bundle ===
     #put_service_bundle /servicebundles/KAFKA ${service_dir}/kafka-bundle.json
 
     #put_service_bundle /servicebundles/STORM ${service_dir}/storm-bundle.json
     #put_service_bundle /servicebundles/ZOOKEEPER ${service_dir}/zookeeper-bundle.json
-    #put_service_bundle /servicebundles/BEAM ${service_dir}/flink-bundle.json
+    put_service_bundle /servicebundles/FLINK ${service_dir}/flink-bundle.json
     #post /servicebundles ${service_dir}/druid-bundle.json
 }
 
